db0dc9169bd177d6f41eb247440804e6113ce638
amilton0656 <amilton0656@gmail.com>
2025-07-23 17:39:34 -0300

    sdf

---
+++ app.py
@@ -0,0 +1,13 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def principal():
+    frutas = ["Morango", "Uva", "Maçã", "Laranja"]
+    return render_template("index.html", frutas=frutas)
+
+@app.route('/sobre')
+def sobre():
+    notas = { "Fulano":5.0, "Beltrano": 6.0, "Aluno": 7.0, "Sicrano": 8.5}
+    return render_template('sobre.html', notas=notas)
